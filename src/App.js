b3fc3ccc5eeacde8cb9d47fbcfdff09ca3c44a0a
Rahul <rahulps1000@gmail.com>
2023-04-14 23:32:50 +0530

    one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [page, setPage] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="form">
+        <div className="title">Registration</div>
+        <div className="steps">
+          <div
+            className={`step one ${page === 1 && "active"}`}
+            onClick={() => setPage(1)}
+          >
+            1
+          </div>
+          <div className="line" />
+          <div
+            className={`step two ${page === 2 && "active"}`}
+            onClick={() => setPage(2)}
+          >
+            2
+          </div>
+        </div>
+        <div className="pages">
+          <div className={`page one ${page === 1 && "active"}`}>
+            <button onClick={() => setPage(2)}>Next</button>
+          </div>
+          <div className={`page two ${page === 2 && "active"}`}>
+            <button onClick={() => setPage(1)}>Prev</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
